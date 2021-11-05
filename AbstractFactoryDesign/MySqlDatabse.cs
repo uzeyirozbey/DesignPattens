064155b844e1bb7bb1d13efc4e8d5737b759b005
uzeyirozbey <ozbey.uzeyir@gmail.com>
2021-11-05 11:19:04 +0300

    deneme
    my deb de değişiklik oldu

--- AbstractFactoryDesign/MySqlDatabse.cs
+++ AbstractFactoryDesign/MySqlDatabse.cs
@@ -3,6 +3,7 @@ namespace AbstractFactoryDesign
 {
     public class MySqlDatabse : DatabaseFactory
     {
+        //deneme tfs
         public override Command CreateCommand() => new MySqlCommand();
         public override Connection CreateConnection() => new MySqlConnection();
     }
